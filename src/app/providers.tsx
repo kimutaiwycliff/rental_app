e36555b94c6345a62b8b4b47a93d1d37da107810
Wycliff Kimutai <99463181+kimutaiwycliff@users.noreply.github.com.>
2025-06-15 08:14:27 +0300

    redux state

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -1,11 +1,14 @@
 'use client';
 
 import { ThemeProvider } from '@/components/providers/theme-provider';
+import { store } from '@/state/store';
 import { TRPCReactProvider } from '@/trpc/client';
+import { Provider } from 'react-redux';
 
 const Providers = ({ children }: { children: React.ReactNode }) => {
   return (
     <TRPCReactProvider>
+      <Provider store={store}>
       <ThemeProvider
         attribute="class"
         defaultTheme="dark"
@@ -14,6 +17,7 @@ const Providers = ({ children }: { children: React.ReactNode }) => {
       >
         {children}
       </ThemeProvider>
+      </Provider>
     </TRPCReactProvider>
   );
 };
